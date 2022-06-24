8b8ceb7bb9790b7281e390c057b77ef73da0a8d1
Redsparrow77 <jayyzero77@gmail.com>
2022-06-24 15:29:17 +0700

    ready to deploy

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,14 @@
-import React from "react"
-
 function Header() {
-    return (
-        <header className="header">
-            <img 
-                src="./images/troll-face.png" 
-                className="header--image"
-                alt="logo"
-            />
-            <h2 className="header--title">Meme Generator</h2>
-            <h4 className="header--project">React Course - Project 3</h4>
-        </header>
-    )
+	return (
+		<header className="header">
+			<img
+				src="./images/troll-face.png"
+				className="header--image"
+				alt="logo"
+			/>
+			<h2 className="header--title">Meme Generator</h2>
+			<h4 className="header--project">Jeheskiel Sunloy</h4>
+		</header>
+	)
 }
 export default Header
